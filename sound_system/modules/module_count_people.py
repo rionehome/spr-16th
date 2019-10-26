dd0c1fd9c42e0a5739cc36437cfa3570b2b3b805
haguremetaru <is0498fr@ed.ritsumei.ac.jp>
2019-10-26 16:14:59 +0900

    first commit

---
+++ sound_system/modules/module_count_people.py
@@ -0,0 +1,11 @@
+from . import module_pico.py
+def count_people(a | b):
+    number = number.split('|')
+    person_number = 'There are {} people, the number of woman'
+
+    return 1
+
+if __name__ == '__main__':
+    w = int(input("number of woman"))
+    m = int(input("number of man"))
+    count(w, m)    
