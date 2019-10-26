769d4289efc0a80eb636003b01cd154af1ed0391
momonga <ts2000kasa@gmail.com>
2019-10-26 22:26:29 +0900

    pacage.xml and setup.py each group

---
+++ sound_system/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+from glob import glob
+
+package_name = 'sound_system'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[],
+    py_modules=[
+        'sound_system'
+    ],
+    install_requires=['setuptools'],
+    data_files=[
+        ('lib/' + package_name, ['package.xml']),
+        ('lib/' + package_name+'/modules',glob('modules/*')),
+        ('lib/sound_system/dictionary/',glob('dictionary/*')),
+        ('lib/sound_system/beep/',glob('beep/*')),
+        ('lib/sound_system/dictionary/QandA',
+            ['dictionary/QandA/qanda.csv']),
+        ('lib/sound_system/log',
+            ['log/log.txt'])
+    ],
+    zip_safe=True,
+    author='momonga',
+    author_email='is0506he@ed.ritsumei.ac.jp'
+    maintainer='momonga',
+    maintainer_email='is0506he@ed.ritsumei.ac.jp
+    keywords=['ROS2'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='sound package for SPR',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'sound_system = sound_system:main',
+        ],
+    },
+)
