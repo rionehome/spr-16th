48102fb7d1a4bea3398e396e9e5390b6cfe0fcaf
Tomoki Mori <reunion0999@gmail.com>
2019-11-09 20:48:12 +0900

    fix bug

--- image_system/image_system.py
+++ image_system/image_system.py
@@ -1,13 +1,8 @@
-from modules import face
-
+from modules import face_detect
+from modules import screenshot
+from std_msgs.msg import String
 import rclpy
 from rclpy.node import Node
-
-from std_msgs.msg import String
-from cv_bridge import CvBridge
-from time import sleep
-from sensor_msgs.msg import Image
-
 
 class ImageSystem(Node):
     def __init__(self):
@@ -16,12 +11,6 @@ class ImageSystem(Node):
         self.senses_publisher = self.create_publisher(
                 String,
                 'cerebrum/command',
-                10
-        )
-
-        self.answor_human_number = self.create_publisher(
-                String,
-                "sound_system/command",
                 10
         )
 
@@ -43,8 +32,6 @@ class ImageSystem(Node):
         self.command = None
         self._trans_message = String()
 
-        self.bridge = CvBridge()
-
         sleep(1)
 
     def command_callback(self, msg):
@@ -56,26 +43,18 @@ class ImageSystem(Node):
         command = msg.data.split(',')
 
         if 'capture' == command[0].replace('Command:', ''):
-            human_number = self.detect_human()
-            print("HUMAN : {0}".format(human_number), flush=True)
-            self.answor_human_number.publish(self._trans_message)
-            #self.cerebrum_publisher('Return:0,Content:')
-
-              
-    # get image from realsense
-    def get_image(self, msg):
-        self.image = self.bridge.imgmsg_to_cv2(msg)
-    # detect number's human
-    def detect_human(self):
-        number = face(self.image)
-        print(number)
-        return number
+            if modules.screenshot() == 1:
+                self.temp_number = modules.face_detect()
+                if self.temp_number >= 0:
+                    self.cerebrum_publisher(
+                        'Return:0,Content:'+str(self.temp_number))
 
     # メッセージ送るやつ
     def cerebrum_publisher(self, message):
 
         self._trans_message.data = message
         self.senses_publisher.publish(self._trans_message)
+
 
 def main():
     rclpy.init()
