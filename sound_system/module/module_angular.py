d026eebf2e232b630bdb6d7b4e603f2d75a46702
Hiroto suzuki <suzuki810459@icloud.com>
2019-11-11 16:19:59 +0900

    完成

--- sound_system/module/module_angular.py
+++ sound_system/module/module_angular.py
@@ -3,10 +3,11 @@ import usb.core
 import usb.util
 import time
 
+
 def angular():
     time.sleep(3)
     dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
-    #print dev
+    # print dev
     if dev:
         Mic_tuning = module_tuning.Tuning(dev)
         while True:
@@ -17,6 +18,7 @@ def angular():
             except KeyboardInterrupt:
                 break
     return Mic_tuning.direction
+
 
 if __name__ == "__main__":
     angular()
