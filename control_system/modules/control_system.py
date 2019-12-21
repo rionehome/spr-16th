ac1a56ce0b3cb2035f74f37218a152c76fb64f3b
Tyutotoro <is0515ss@ed.ritsumei.ac.jp>
2019-12-21 15:06:33 +0900

    たぶんできるはず

--- control_system/modules/control_system.py
+++ control_system/modules/control_system.py
@@ -5,7 +5,7 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Quaternion
 from time import sleep
-
+import re
 from numpy import *
 
 class ControlSystem(Node):
@@ -25,22 +25,31 @@ class ControlSystem(Node):
             10
         )
 
+        self.create_subscription(
+            String,
+            '/control_system/command',
+            self.subscribe_command,
+            10
+        )
+
+        self.command_publisher = self.create_publisher(
+            String,
+            '/cerebrum/command',
+            10
+        )
+
         self.velocity_publisher = self.create_publisher(Twist, "/turtlebot2/commands/velocity",10)
 
         self.reset_publisher = self.create_publisher(Bool, "/turtlebot2/commands/reset_pose", 10)
 
         self.twist = Twist()
 
-        self.goal_degree = 180
+        #self.goal_degree = 180
 
         self.is_running=False
 
         self.angle=0
 
-    """
-    this method calcurates angle by odometry.
-    odometry から角度を計算する。
-    """
     def get_angle_by_pose(self, pose):
         z = pose.pose.orientation.z 
         w = pose.pose.orientation.w #w,zはクオータニオン表記　
@@ -53,11 +62,11 @@ class ControlSystem(Node):
             angle =  arccos(w)*360 / pi
 
         return angle
+    """
+    this method calcurates angle by odometry.
+    odometry から角度を計算する。
+    """
 
-    """
-    this method is to chang angle.
-    止まる角度を-180~180の間に変換する。
-    """
     def normalize_goal_degree(self, goal_degree):
         goal_degree %= (360 if goal_degree>0 else -360) 
 
@@ -74,44 +83,70 @@ class ControlSystem(Node):
         print("GOAL: " + str(goal_degree))
         
         return goal_degree
+    """
+    this method is to chang angle.
+    止まる角度を-180~180の間に変換する。
+    """
 
-    """
-    this method is callback of `/turtlebot2/odometry` topic. 
-    this method is judgment of stopping turtlebot
-    `/turtlebot2/odometry` トピックのコールバック。
-    turtlebot の停止の判定
-    """
     def odometry_subscriber(self, msg):
         angle = self.get_angle_by_pose(msg.pose)
         print(angle)
         
         if (self.goal_degree > 0 and angle > self.goal_degree) or (self.goal_degree < 0 and angle < self.goal_degree):
             self.stop_turtlebot()
+    """
+    this method is callback of `/turtlebot2/odometry` topic. 
+    this method is judgment of stopping turtlebot
+    `/turtlebot2/odometry` トピックのコールバック。
+    turtlebot の停止の判定
+    """
 
+    def subscribe_command(self,msg):
+        command,content = self.command_str(msg.data)
+        if command =="turn":
+            self.turn_to(int(content),30.0)
     """
-    the method let turtlebot stopping. 
-    turtlebot を止まらせる。
+
+    CICから受け取る
     """
+
+    def parse_command(self,commad_str):
+        m = re.match(r"Command:([a-z]+),Content:(.+):cerebrum")
+        return m.groups()
+    """
+
+    データの読み取り
+    """
+
     def stop_turtlebot(self):
         self.twist.angular.z = 0.0
         print("STOP TURTLEBOT")
         self.is_running=False
         self.velocity_publisher.publish(self.twist)
+        self.cerebrum_publisher('Return:0,Content:True')
+    """
+    the method let turtlebot stopping. 
+    turtlebot を止まらせる。
+    """
 
+    def cerebrum_publisher(self, message_str):
+        msg_str = String()
+        msg_str.data = message_str
+        self.command_publisher.publish(msg_str)
     """
-    this method set pose of turtlebot to (0, 0, 0).
-    turtlebot のポーズを(0, 0, 0)にセットする。
+    データを送る
     """
+
     def reset_pose(self):
         reset_flag = Bool()
         reset_flag.data = True
         self.reset_publisher.publish(reset_flag)
         print("pose rested")
+    """
+    this method set pose of turtlebot to (0, 0, 0).
+    turtlebot のポーズを(0, 0, 0)にセットする。
+    """
 
-    """
-    this method let turtlebot running.
-    Turtlebot を走らせる。
-    """
     def turn_to(self, goal_degree, angular_speed):
         if (not self.is_running) and (angular_speed==0) :     #止まってる際に(速さ)0を連続で送らないようにする
             return
@@ -129,26 +164,17 @@ class ControlSystem(Node):
 
         self.velocity_publisher.publish(self.twist)
         print("running...")
+    """
+    this method let turtlebot running.
+    Turtlebot を走らせる。
+    """
 
 def main():
     rclpy.init()
 
     node = ControlSystem()
 
-    sleep(1)
-    node.turn_to(180, 30.0)
-
-    #rclpy.spin(node)
-    #node.destroy_node()
-    #rclpy.shutdown()
-
-    sleep(1)
-    node.turn_to(90, 20.0)
-
     rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
-
 
 if __name__ == "__main__":
     main()
